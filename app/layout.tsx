ecf48618f62882956137d63059989392370517c0
nipunnegi2 <nipunnegi2002@gmail.com>
2025-11-16 22:26:36 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/apple-icon.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/apple-icon.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
